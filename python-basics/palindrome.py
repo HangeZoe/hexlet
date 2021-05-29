67fd7f88cbdccecd6aaa2f22067bfc94835b6663
ハンジ・ゾエ <60126308+HangeZoe@users.noreply.github.com>
2021-05-29 21:20:38 +0200

    feat: update palindrome.py

--- python-basics/palindrome.py
+++ python-basics/palindrome.py
@@ -1,1 +1,8 @@
 # Реализуйте функцию is_palindrome(), которая принимает на вход слово, определяет является ли оно палиндромом и возвращает логическое значение.
+
+def is_palindrome(string):
+  reverse_string = string[::-1]
+  if reverse_string == string:
+    return True
+  else:
+    return False
