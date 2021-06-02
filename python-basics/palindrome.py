7f2ac498d875e79f83695f7c38202494df44ed50
ハンジ・ゾエ <60126308+HangeZoe@users.noreply.github.com>
2021-06-02 23:50:53 +0200

    docs: comment function

--- python-basics/palindrome.py
+++ python-basics/palindrome.py
@@ -1,8 +1,19 @@
-# Реализуйте функцию is_palindrome(), которая принимает на вход слово, определяет является ли оно палиндромом и возвращает логическое значение.
+# Реализуйте функцию is_palindrome(), которая принимает на вход слово, 
+# определяет является ли оно палиндромом и возвращает логическое значение.
+
 
 def is_palindrome(string):
+  """Функция определяет, является ли введеное слово палиндромом
+  и возвращает истину, если это так
+  """
   reverse_string = string[::-1]
   if reverse_string == string:
     return True
   else:
     return False
+
+  
+# Another solution:
+#
+# def is_palindrome(string):
+#     return str(string)[::-1] == str(string)
