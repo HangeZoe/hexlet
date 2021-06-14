2fd10ce7a94fdcdc4750a257507834841a114c39
ハンジ・ゾエ <60126308+HangeZoe@users.noreply.github.com>
2021-06-14 05:28:20 +0200

    feat: add challenge function

--- python-lists/longest_substring.py
+++ python-lists/longest_substring.py
@@ -4,3 +4,14 @@
 # Подстрока может состоять из одного символа. Например в строке qweqrty
 # можно выделить следующие подстроки: qwe, weqrty. 
 # Самой длинной будет weqrty, а её длина — 6 символов.
+
+
+def find_longest_length(string):
+  """Принимает строку и возвращает
+  число уникальных символов
+  """
+  length = len(string) + 1
+  for i in string:
+    if string.count(i) > 1:
+      length -= 1
+  return length
